49238e8ded73581d0caf87958900950c953b78f1
Sayantan Das <sayantandasbhp@gmail.com>
2022-06-12 16:21:16 +0530

    Basic Codes, first Commit

---
+++ Project1/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello World Project 1" << endl;
+    return 0;
+}
---
+++ Project2/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num{100};
+    cout << "Hello World Project 2" << endl;
+    cout << "Enter your Favorite Number";
+    cin >> num;
+    cout << "Your Favorite Number is " << num << endl;
+    return 0;
+}
